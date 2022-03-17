68c23523fa0e7c8a48b58fd30703b1369dd62b37
Patrick Yeh <patrickyeh@cht.com.tw>
2022-03-17 01:19:44 -0700

    adjust install script

--- install_deepflow.sh
+++ install_deepflow.sh
@@ -7,10 +7,12 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends tzdata
 ### 
 # parameters
 ###
+OP_USER="tl_admin"
 HOST_IP=$(hostname -I | awk '{print $1}')
 DOCKER_COMPOSE_VERSION=1.29.2
-PACKAGES=("curl" "openssh-server" "nfs-common" "nfs-kernel-server" "gnupg2" "pass" "nvidia-container-runtime" )
-
+PACKAGES=("net-tools","curl" "openssh-server" "nfs-common" "nfs-kernel-server" "gnupg2" "pass" "nvidia-container-runtime" )
+USER_HOME=/home/${OP_USER}
+AIPAAS_HOME=${USER_HOME}/workspace/aipaas
 
 DEBIAN_FRONTEND=noninteractive
 TZ=Etc/UTC
@@ -28,14 +30,32 @@ echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 ### 
 # Create operation user
 ###
-OP_USER="tl_admin"
+
 sudo useradd -m -s /bin/bash -G sudo ${OP_USER}
 # sudo passwd ${OP_USER}
+
+
+### create workspace
+mkdir /home/${OP_USER}/workspace
+
 
 ### 
 # Install packages
 ###
 
+#install python 3.9
+sudo apt install -y software-properties-common
+sudo apt install -y python3.9
+sudo apt install -y python3-pip
+#install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+pip install docker-compose
 
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
@@ -60,99 +80,79 @@ ssh-keyscan -H $HOST_IP >> /home/$OP_USER/.ssh/known_hosts
 
 echo exit | ssh "$OP_USER@$HOST_IP"
 
-# Download images
-PORTAL_IMAGES=( \
-        ############ AIPAAS元件 ############
-        "kong:custom" \
-        "django:latest" \
-        "aipaas_aipaas_frontend:latest" \
-        "aipaas_nginx:latest" \
-        "aipaas/aipaas_fluentd:1.9.1" \
-        "aipaas_explain:v1.0" \
-        "file_profiling:v1.0" \
-        "postgres:13" \
-        "pantsel/konga:next" \
-        "bitnami/redis:latest" \
-        "portainer/portainer:latest" \
-        "grafana/grafana:latest" \
-        "prom/prometheus:latest" \
-        "prom/node-exporter:v1.0.0" \
-        "google/cadvisor:v0.33.0" \
-        "gpustatus-exporter:latest" \
-        ####################################
-        ############ 模型健康度 #############
-        "mongo:4.2.9" \
-        "aipaas/aipaas_model_health_agent:1.0" \
-        "aipaas/aipaas_model_health_flask:1.0" \
-        ####################################
-        ############ 財稅案 #################
-        # "registry.gitlab.com/chunghwatelecom/ai/fiadata:latest" \
-        # "postgres:9.6-alpine" \
-        ####################################
-        )
 
-NODE_IMAGES=( \
-        ############ 運算機資源監控 #########
-        "prom/node-exporter:v1.0.0" \
-        "google/cadvisor:v0.33.0" \
-        "gpustatus-exporter:latest" \
-        ####################################
-        ############ Develop ###############
-        "nvcr.io/nvidia/tensorflow:21.06-tf2-py3-aipaas2.5" \
-        "nvcr.io/nvidia/pytorch:21.06-py3-aipaas2.5" \
-        "nvcr.io/nvidia/mxnet:21.06-py3-aipaas2.5" \
-        ####################################
-        ############ Harbor ###############
-        "goharbor/redis-photon:v2.2.2" \
-        "goharbor/trivy-adapter-photon:v2.2.2" \
-        "goharbor/harbor-registryctl:v2.2.2" \
-        "goharbor/registry-photon:v2.2.2.3MB" \
-        "goharbor/nginx-photon:v2.2.2.4MB" \
-        "goharbor/harbor-log:v2.2.2" \
-        "goharbor/harbor-jobservice:v2.2.2" \
-        "goharbor/harbor-core:v2.2.2" \
-        "goharbor/harbor-portal:v2.2.2.1MB" \
-        "goharbor/harbor-db:v2.2.2" \
-        "goharbor/prepare:v2.2.2" \
-        ####################################
-        ############ AutoML ################
-        "aipaas/automl:cuda10.2-py37" \
-        ####################################
-        ############ 數據探索 ###############
-        "file_profiling:v1.0" \
-        ####################################
-        ############ 模型部署 ###############
-        "mlflow_cpu:latest" \
-        "bentoml:v1" \
-        ####################################
-        )
 
-images_list=`docker images --format "{{.Repository}}:{{.Tag}}"`
-echo "Download deepflow images"
-mkdir -p ~/workspace/aipaas-offline/portal
-for ((i=0; i < ${#PORTAL_IMAGES[@]}; i++))
-do
-    fname=$(echo ${PORTAL_IMAGES[$i]} | sed 's/[/:]/_/g')
-    wget -P ~/workspace/aipaas-offline/portal/ ${IMAGE_URL_PREFIX}portal/${fname}.tar
-done
+# NODE_IMAGES=( \
+#         ############ 運算機資源監控 #########
+#         "prom/node-exporter:v1.0.0" \
+#         "google/cadvisor:v0.33.0" \
+#         "gpustatus-exporter:latest" \
+#         ####################################
+#         ############ Develop ###############
+#         "nvcr.io/nvidia/tensorflow:21.06-tf2-py3-aipaas2.5" \
+#         "nvcr.io/nvidia/pytorch:21.06-py3-aipaas2.5" \
+#         "nvcr.io/nvidia/mxnet:21.06-py3-aipaas2.5" \
+#         ####################################
+#         ############ Harbor ###############
+#         "goharbor/redis-photon:v2.2.2" \
+#         "goharbor/trivy-adapter-photon:v2.2.2" \
+#         "goharbor/harbor-registryctl:v2.2.2" \
+#         "goharbor/registry-photon:v2.2.2.3MB" \
+#         "goharbor/nginx-photon:v2.2.2.4MB" \
+#         "goharbor/harbor-log:v2.2.2" \
+#         "goharbor/harbor-jobservice:v2.2.2" \
+#         "goharbor/harbor-core:v2.2.2" \
+#         "goharbor/harbor-portal:v2.2.2.1MB" \
+#         "goharbor/harbor-db:v2.2.2" \
+#         "goharbor/prepare:v2.2.2" \
+#         ####################################
+#         )
 
-mkdir -p ~/workspace/aipaas-offline/node
-for ((i=0; i < ${#NODE_IMAGES[@]}; i++))
-do
-    fname=$(echo ${NODE_IMAGES[$i]} | sed 's/[/:]/_/g')
-    wget -P ~/workspace/aipaas-offline/node ${IMAGE_URL_PREFIX}portal/${fname}.tar
-done
+# images_list=`docker images --format "{{.Repository}}:{{.Tag}}"`
 
-# portal
-cd ~/workspace/aipaas-offline
-for f in ./portal/*.tar; do
-    echo -e "${f} ... ..."
-    cat $f | docker load
-done
 
-# node
-cd ~/workspace/aipaas-offline
-for f in ./node/*.tar; do
-    echo -e "${f} ... ..."
-    cat $f | docker load
-done
+# mkdir -p ~/workspace/aipaas-offline/node
+# for ((i=0; i < ${#NODE_IMAGES[@]}; i++))
+# do
+#     fname=$(echo ${NODE_IMAGES[$i]} | sed 's/[/:]/_/g')
+#     wget -P ~/workspace/aipaas-offline/node ${IMAGE_URL_PREFIX}portal/${fname}.tar
+# done
+
+# # portal
+# cd ~/workspace/aipaas-offline
+# for f in ./portal/*.tar; do
+#     echo -e "${f} ... ..."
+#     cat $f | docker load
+# done
+
+# # node
+# cd ~/workspace/aipaas-offline
+# for f in ./node/*.tar; do
+#     echo -e "${f} ... ..."
+#     cat $f | docker load
+# done
+
+
+
+###
+# Download deepflow program
+###
+
+## download
+wget -P ${USER_HOME}/workspace/deepflow.tar.gz http://web.ctyeh.com/deepflow.tar.gz
+cd ${USER_HOME}/workspace/
+tar zxvf FileName.tar.gz
+# to be define
+
+
+## initiallize
+
+# cp local.py
+cp ${AIPAAS_HOME}/backend/src/aipaas2/.env.sample ${AIPAAS_HOME}/backend/src/aipaas2/.env
+cp ${AIPAAS_HOME}/backend/src/aipaas2/settings/local.azure.py ${AIPAAS_HOME}/backend/src/aipaas2/settings/local.py 
+cp ${AIPAAS_HOME}/build_aipaas/install_script_shell/install_script_azure.sh ${AIPAAS_HOME}/install_script_azure.sh
+cp ${AIPAAS_HOME}/build_aipaas/install_docker_compose/docker-compose-cloud.yml ${AIPAAS_HOME}/docker-compose.yml
+sudo chmod +x ${AIPAAS_HOME}/install_script_azure.sh
+# run aipaas installation
+cd ${AIPAAS_HOME}
+sudo sh install_script_azure.sh
